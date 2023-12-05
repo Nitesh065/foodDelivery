019d3399291e81120c9bf35c4df4afe11f8dc18e
Nitesh <niteshwadhwani605@gmail.com>
2023-12-05 15:04:32 +0530

    changed bottomnavigating by using navigation.xml for changing the fragments

--- app/src/main/java/com/example/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/example/fooddelivery/MainActivity.kt
@@ -3,32 +3,41 @@ package com.example.fooddelivery
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.fooddelivery.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var homeFragment = Home()
-    private var cartFragment = Cart()
-    private var menuFragment = menu()
-    private var userFragment = user()
+
+//    private var homeFragment = Home()
+//    private var cartFragment = Cart()
+//    private var menuFragment = menu()
+//    private var userFragment = user()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        loadFragment(homeFragment)
+    var navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHostFragment
+    var navController = navHostFragment.navController
+    var bottomNav: BottomNavigationView = findViewById(R.id.bottom_navigation)
+    bottomNav.setupWithNavController(navController)
+//        loadFragment(homeFragment)
 
-        binding.bottomNavigation.setOnItemSelectedListener {item ->
-            when(item.itemId){
-                R.id.navigation_home -> loadFragment(homeFragment)
-               R.id.navigation_cart -> loadFragment(cartFragment)
-               R.id.navigation_menu -> loadFragment(menuFragment)
-                R.id.navigation_user -> loadFragment(userFragment)
-            }
-            true
-        }
+//        binding.bottomNavigation.setOnItemSelectedListener {item ->
+//            when(item.itemId){
+//                R.id.navigation_home -> loadFragment(homeFragment)
+//               R.id.navigation_cart -> loadFragment(cartFragment)
+//               R.id.navigation_menu -> loadFragment(menuFragment)
+//                R.id.navigation_user -> loadFragment(userFragment)
+//            }
+//            true
+//        }
     }
-    private fun loadFragment(fragment: Fragment){
-        supportFragmentManager.beginTransaction().replace(R.id.frameLayout,fragment).commit()
-    }
+
 }
